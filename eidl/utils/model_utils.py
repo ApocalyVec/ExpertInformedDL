9eff34ca2e7acb6715e1ddabce2470c8fb72a9f4
Ziheng 'Leo' Li <apocalyvec@gmail.com>
2024-01-04 01:00:34 -0500

    update file tree

--- eidl/utils/model_utils.py
+++ eidl/utils/model_utils.py
@@ -83,7 +83,7 @@ def get_trained_model(device, model_param):
     else:
         raise ValueError(f"model_param {model_param} is not supported")
 
-    github_file_url = "https://raw.githubusercontent.com/ApocalyVec/ExpertInformedDL/master/trained_model/0.0.1"
+    github_file_url = "https://raw.githubusercontent.com/ApocalyVec/ExpertInformedDL/master/eidl/trained_model/0.0.1"
     model_url = f"{github_file_url}/trained_model/best_model-base_alpha-0.01_dist-cross-entropy_depth-1_lr-0.0001_statedict_{model_param}.pt"
     image_mstd_url = f"{github_file_url}/image_means_stds_{model_param}.p"
     compound_label_encoder_url = f"{github_file_url}/compound_label_encoder.p"
@@ -108,8 +108,8 @@ def get_trained_model(device, model_param):
     return model, image_mean, image_std, image_size, compound_label_encoder
 
 def get_subimage_model():
-    github_file_url = "https://raw.githubusercontent.com/ApocalyVec/ExpertInformedDL/master/trained_model/0.0.2"
-    model_url = f"{github_file_url}/trained_model/model.pt"
+    github_file_url = "https://raw.githubusercontent.com/ApocalyVec/ExpertInformedDL/master/eidl/trained_model/0.0.2"
+    model_url = f"{github_file_url}/model.pt"
     temp_dir = tempfile.gettempdir()
     model_file_path = os.path.join(temp_dir, "subimage_model.pt")
     dataset_path = os.path.join(temp_dir, "oct_reports_info.p")
