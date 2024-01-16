f937e0e78c4109051637de4aa3aaff3dfb1ae0b9
Ziheng 'Leo' Li <apocalyvec@gmail.com>
2024-01-15 20:54:16 -0500

    update training script for srever

--- main_oct_v3.py
+++ main_oct_v3.py
@@ -36,8 +36,8 @@ cropped_image_data_path = '/home/leo/Data/oct_v2/oct_reports_info_repaired.p'
 results_dir = '../temp/results'
 # use_saved_folds = None
 # use_saved_folds = '../temp/results-repaired-base-vit'
-use_saved_folds = '../temp/results-repaired-pretrained-vit'
-# use_saved_folds = '../temp/results-repaired-inception'
+# use_saved_folds = '../temp/results-repaired-pretrained-vit'
+use_saved_folds = '../temp/results-repaired-inception'
 
 
 n_jobs = 20  # n jobs for loading data from hard drive and z-norming the subimages
@@ -81,9 +81,9 @@ aoi_loss_distance_types = 'cross-entropy',
 ################################################################
 # model_names = 'base', 'vit_small_patch32_224_in21k', 'vit_small_patch16_224_in21k', 'vit_large_patch16_224_in21k'
 # model_names = 'base', 'vit_small_patch32_224_in21k'
-model_names = 'vit_small_patch32_224_in21k_subimage',
+# model_names = 'vit_small_patch32_224_in21k_subimage',
 # model_names = 'base_subimage',
-# model_names = 'inception_v4_subimage',
+model_names = 'inception_v4_subimage',
 
 grid_search_params = {
     'vit_small_patch32_224_in21k_subimage': {
